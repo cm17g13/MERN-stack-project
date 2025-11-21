fd9a2ebe26885ad985008caf8f4d020e0d28654f
Callum McGregor <callum.mcgregor@bp.com>
2025-11-21 13:46:38 +0000

    created the Notes model

---
+++ backend/src/models/Note.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+// 1. create a schema
+// 2. create model from schema
+
+// the formats of the data stored in mongodb
+const noteSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        }
+    },
+    { timestamps: true }
+);
+
+const Note = mongoose.model("Note", noteSchema)
+
+export default Note;
