904fd05619508e0c4e90a5f515928600d529cfd2
Callum McGregor <callum.mcgregor@bp.com>
2025-12-01 17:34:01 +0000

    added tailwind and daisyUI elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,19 @@ import { Route, Routes } from "react-router";
 import HomePage from './pages/Homepage';
 import CreatePage from './pages/CreatePage';
 import NoteDetailPage from './pages/NoteDetailPage';
+import toast from 'react-hot-toast';
 
 const App = () => {
-  return <div>
+  return <div data-theme = "synthwave">
+    {/*<button onClick={ () => toast.success("Congrats") } className="text-purple-600 bg-pink-200 p-4">Click This</button>
+    <button onClick={ () => toast.success("Congrats") } className="btn btn-outline text">Click This</button>
+    <button className="btn">Button</button>
+    <button className="btn btn-neutral">Neutral</button>
+    <button className="btn btn-primary">Primary</button>
+    <button className="btn btn-secondary">Secondary</button>
+    <button className="btn btn-accent">Accent</button>
+    <button className="btn btn-ghost">Ghost</button>
+    <button className="btn btn-link">Link</button> */}
     <Routes>
       <Route path="/" element ={<HomePage />} />
       <Route path="/create" element ={<CreatePage />} />
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+
+
+  }, []);
+
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+    </div>
+  );
 };
 
 export default HomePage
